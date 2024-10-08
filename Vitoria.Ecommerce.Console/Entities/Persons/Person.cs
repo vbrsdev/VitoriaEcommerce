df61e2565eb5447e2a3d76a2068a8a2685813d5b
Vitória Beatriz <vbrsvitoria@gmail.com>
2024-10-08 20:45:35 -0300

    Projeto Inicial

---
+++ Vitoria.Ecommerce.Console/Entities/Persons/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitoria.Ecommerce.Console.Entities.Persons
+{
+    public class PersonRead   //criando classe no C#
+    {
+        public int PersonId { get; set; } //propriedade
+
+        public int PersonType { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string PersonCPFCNPJ { get; set; }
+
+        public string PersonAddress { get; set; }
+
+    }
+
+    public class PersonCreate
+    {
+        public int PersonType { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string PersonCPFCNPJ { get; set; }
+
+        public string PersonAddress { get; set; }
+
+    }
+
+    public class PersonUpdate   //criando classe no C#
+    {
+        public int PersonId { get; set; } //propriedade
+
+        public int PersonType { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string PersonCPFCNPJ { get; set; }
+
+        public string PersonAddress { get; set; }
+
+    }
+
+
+}
---
+++ Vitoria.Ecommerce.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using Vitoria.Ecommerce.Console.Entities.Persons;
+
+List<PersonRead> personList = new List<PersonRead>();
+List<PersonCreate> personCreateList = new List<PersonCreate>();
+
+// int count = 10;
+
+// for (int i = 1; i <= count; i++)
+// {
+//     personCreateList.Add(new PersonCreate { PersonAddress = $"Rua Além {i}"});
+// }
+
+
+
+
+List<int> PersonIDS = new List<int>();
+
+int index = 12;
+for(int i = 1; i < index; i++)
+{
+    PersonIDS.Add(i);
+}
